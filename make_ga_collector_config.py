7c6eb3cb8cdb647f7362d7a88e6481bb56fcd072
Peter Waller <peter@scraperwiki.com>
2014-03-19 10:03:00 +0000

    Start writing files out in parts

--- make_ga_collector_config.py
+++ make_ga_collector_config.py
@@ -83,24 +83,39 @@ def has_no_unexpected_keys(ga_config):
         result = False
     return result, unexpected_keys
 
+
 def load_csv_as_json(path):
     """
     Read a csv file into json dictionaries, using the first row as keys
     """
     with open(path) as fd:
         rows = list(csv.reader(fd))
-        headings = rows.pop(0)
 
-        return [dict(zip(headings, values)) for values in rows]
+    headings = rows.pop(0)
+    return [dict(zip(headings, values)) for values in rows]
+
+
+def output_bucket_config(row):
+    row["dataType"]
+
+
+def output_ga_collector_config(row):
+    pass
+
+
+def output_spotlight_config(row):
+    # TODO(pwaller):
+    pass
+
 
 def main(args):
 
     INPUT_CSV_PATH = "collector-config/GoogleAnalyticsQueries - Searches.csv"
 
-    from pprint import pprint
-    pprint(load_csv_as_json(INPUT_CSV_PATH))
-
-    return
+    for row in load_csv_as_json(INPUT_CSV_PATH):
+        output_bucket_config(row)
+        output_ga_collector_config(row)
+        output_spotlight_config(row)
 
 
 if __name__ == '__main__':
