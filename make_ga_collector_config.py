58ee1f62ef0afc65a56f40a6bc5b348c0531b736
IanHopkinson <ian@scraperwiki.com>
2014-03-26 07:43:51 +0000

    Add templating for content dashboards, hard-coded to generate one dashboard for DFT

--- make_ga_collector_config.py
+++ make_ga_collector_config.py
@@ -16,7 +16,9 @@ from __future__ import unicode_literals
 import codecs
 import collections
 import csv
+import errno
 import json
+import os
 import sys
 
 import docopt
@@ -120,13 +122,33 @@ def output_ga_collector_config(row, args):
     row["bearer_token"] = "scraperwiki"
     row["backdrop_target"] = "http://localhost:3039/data"
 
+    try:
+        os.makedirs("collector-config/output")
+    except OSError as e:
+        if e.errno!=errno.EEXIST:
+            raise
+
     with open("collector-config/output/{}.json".format(row["dataType"]), "w") as fd:
         fd.write(template.render(**row))
 
 
-def output_spotlight_config(row):
+def output_spotlight_config(ga_row, args):
     # TODO(pwaller):
-    pass
+    row = {}
+    template = jinja.from_string(open("spotlight-config/content-dashboard-template.json").read())
+
+    row["dashboard_config_name"] = "dft-content-dashboard"
+    row["dept_name"] = "Department for Transport"
+    row["dept_abbrev"] = "DFT"
+    row["dept_slug"] = "department-for-transport"
+    try:
+        os.makedirs("spotlight-config/output")
+    except OSError as e:
+        if e.errno!=errno.EEXIST:
+            raise
+
+    with open("spotlight-config/output/{}.json".format(row["dashboard_config_name"]), "w") as fd:
+        fd.write(template.render(**row))
 
 
 def main(args):
@@ -136,7 +158,7 @@ def main(args):
     for row in load_csv_as_json(INPUT_CSV_PATH):
         output_bucket_config(row)
         output_ga_collector_config(row, args)
-        output_spotlight_config(row)
+        output_spotlight_config(row, args)
 
 
 if __name__ == '__main__':
